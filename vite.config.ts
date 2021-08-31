72288ba853ee9333cafd6ded05d6f62bfee88a85
xiyanjo <xiyanjo@163.com>
2021-08-31 18:25:42 +0800

    跟新文件夹，实现瀑布流

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
       'styles': path.resolve(__dirname, 'src/styles'),
       'utils':path.resolve(__dirname, 'src/utils'),
     },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],//这加上.vue
   },
   plugins: [vue()]
 })
